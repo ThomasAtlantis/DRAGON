aab5c7675bf85310d5d31915cf913421fb480589
ThomasAtlantis <1138670081@qq.com>
2025-03-26 13:31:01 +0800

    save experiment results

---
+++ outputs/LanguageModeling-20250321220238/run.sh
@@ -0,0 +1,15 @@
+python -u experiments/LanguageModeling/eval.py \
+    --retriever.passages Salesforce/wikitext,wikitext-103-raw-v1 \
+    --retriever.passages_embeddings data/wikitext103/*.pkl \
+    --retriever.s_context 256 \
+    --generator.model facebook/opt-1.3b \
+    --generator.s_sequence 1024 \
+    --evaluator.s_prefix 128 \
+    --evaluator.dataset Salesforce/wikitext,wikitext-103-raw-v1 \
+    --evaluator.output_dir outputs/ \
+    --cache.load_index \
+    --cache.load_query2docs \
+    --generator.use_fp16 \
+    --retriever.n_docs 5 \
+    --retriever.s_aggregate 1 \
+    --retriever.downsample_type 1
---
+++ outputs/LanguageModeling-20250322194829/run.sh
@@ -0,0 +1,15 @@
+python -u experiments/LanguageModeling/eval.py \
+    --retriever.passages Salesforce/wikitext,wikitext-2-raw-v1 \
+    --retriever.passages_embeddings data/wikitext2/*.pkl \
+    --retriever.s_context 256 \
+    --generator.model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
+    --generator.s_sequence 640 \
+    --evaluator.s_prefix 64 \
+    --evaluator.dataset Salesforce/wikitext,wikitext-2-raw-v1 \
+    --evaluator.output_dir outputs/ \
+    --cache.load_index \
+    --cache.load_query2docs \
+    --generator.use_fp16 \
+    --retriever.n_docs 0 \
+    --retriever.s_aggregate 0 \
+    --retriever.downsample_type 0
